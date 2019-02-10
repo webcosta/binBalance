93df03d9af60383a4e97b510285170da3325936c
Webcosta <webkosta@gmail.com>
2019-02-10 17:55:27 +0200

    feat(Core): Service Classes and Workers added. Model updated

--- app/models/request_result.rb
+++ app/models/request_result.rb
@@ -1,2 +1,10 @@
 class RequestResult < ApplicationRecord
+
+    after_commit :launchParserWorker, on: :create
+
+    def launchParserWorker
+        puts "Entry saved with id:#{id}"
+        puts "updating balances..."
+        BinanceParserWorker.perform_async(id)
+    end
 end
--- app/services/binance_parser_service.rb
+++ app/services/binance_parser_service.rb
@@ -1,15 +1,34 @@
 class BinanceParserService
     
 
-    def initialize
+    def initialize(entry_id)
+        @entry_id =  entry_id
     end
 
     def parse_data
-        test_message
+
+        raw_data = RequestResult.where(id:@entry_id).select("raw_data").as_json
+        raw_data_decoded = raw_data[0]["raw_data"]
+        json_balances = JSON.parse(raw_data_decoded)
+        balances = []
+        balancesArr = json_balances["balances"] 
+        balancesArr.each {|asset| 
+            if asset["free"].to_f > 0
+                balances.push({asset: asset["asset"], free: asset["free"]})
+            end
+        }
+        
+        updateEntry(balances, @entry_id) unless balances.empty?
+        
     end
 
     private
-    def test_message
-        puts "Parser is OK:------------------------------------------------+"
+    def updateEntry(parsed_data, entry_id)
+        entry = RequestResult.find(entry_id)
+        if entry.parsed_data.present?
+        else
+            entry.update(parsed_data: parsed_data)
+            puts "Balances updated"
+        end
     end
 end
